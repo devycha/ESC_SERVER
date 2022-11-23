4346a28555b003c546191c982849bba372c8edba
dongjji <y2kdj9723@naver.com>
2022-11-23 20:10:08 +0900

    Project Setup

---
+++ src/main/java/com/minwonhaeso/esc/EscServerApplication.java
@@ -0,0 +1,13 @@
+package com.minwonhaeso.esc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EscServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EscServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/minwonhaeso/esc/EscServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minwonhaeso.esc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EscServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
