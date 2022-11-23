87ef6eb492837628b03986896dc647eee49a8687
dongjji <y2kdj9723@naver.com>
2022-11-23 20:18:14 +0900

    test

--- src/main/java/com/minwonhaeso/esc/MainController.java
+++ src/main/java/com/minwonhaeso/esc/MainController.java
@@ -9,4 +9,9 @@ public class MainController {
     public String hello() {
         return "hello";
     }
+
+    @GetMapping("/hello2")
+    public String hello2() {
+        return "hello2";
+    }
 }
