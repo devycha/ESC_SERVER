729bb48c1ca29a689a58ffd98df259cd583acef2
chandle <phc09188@gmail.com>
2022-12-06 19:19:36 +0900

    emailAuthKey보내는 클래스 이름 변경

--- src/main/java/com/minwonhaeso/esc/security/auth/AuthUtil.java
+++ src/main/java/com/minwonhaeso/esc/security/auth/AuthUtil.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class AuthUtil {
 
-    public String generateAuthNo() {
+    public String generateEmailAuthNum() {
         java.util.Random generator = new java.util.Random();
         generator.setSeed(System.currentTimeMillis());
         return String.valueOf(generator.nextInt(1000000) % 1000000);
