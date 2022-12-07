cdc75c703a8d79c527d0d8db92da220dbb9f028c
chandle <phc09188@gmail.com>
2022-12-07 10:23:36 +0900

    ElasticsearchConfig 중괄호 안 닫혀있는 부분 수정 Auth 부분 Controller Swagger 정보 추가

--- src/main/java/com/minwonhaeso/esc/member/controller/MemberController.java
+++ src/main/java/com/minwonhaeso/esc/member/controller/MemberController.java
@@ -5,6 +5,7 @@ import com.minwonhaeso.esc.member.model.dto.SignDto;
 import com.minwonhaeso.esc.member.model.dto.TokenDto;
 import com.minwonhaeso.esc.member.service.MemberService;
 import com.minwonhaeso.esc.security.auth.jwt.JwtTokenUtil;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -21,6 +22,7 @@ public class MemberController {
     /**
      * 회원가입
      **/
+    @ApiOperation(value = "회원 가입", notes = "입력한 정보로 회원가입한다.")
     @PostMapping("/signUp")
     public ResponseEntity<?> signUp(@RequestBody SignDto.Request signDto) {
         SignDto.Response response = memberService.signUser(signDto);
@@ -30,6 +32,7 @@ public class MemberController {
     /**
      * 회원가입 이메일 중복 검사
      **/
+    @ApiOperation(value = "이메일 중복 확인", notes = "입력 받은 이메일의 회원이 이미 존재하는지 확인한다.")
     @PostMapping("/email-dup")
     public ResponseEntity<?> emailDuplicated(@RequestBody Map<String, String> email) {
         memberService.emailDuplicateYn(email.get("email"));
@@ -39,6 +42,7 @@ public class MemberController {
     /**
      * 회원가입 이메일 인증 코드 전송
      **/
+    @ApiOperation(value = "이메일 인증 코드 전송", notes = "이메일로 회원가입을 위한 코드를 전송합니다.")
     @PostMapping("/email-auth")
     public ResponseEntity<?> deliverEmailAuthCode(@RequestBody Map<String, String> email) {
         memberService.deliverEmailAuthCode(email.get("email"));
@@ -49,6 +53,7 @@ public class MemberController {
     /**
      * 메일 인증
      **/
+    @ApiOperation(value = "메일 인증", notes = "메일 인증 코드가 맞는지 확인합니다.")
     @PostMapping("/email-authentication")
     public ResponseEntity<?> emailAuthentication(@RequestParam String key) {
         memberService.emailAuthentication(key);
@@ -58,6 +63,7 @@ public class MemberController {
     /**
      * 로그인
      **/
+    @ApiOperation(value = "로그인", notes = "일반 계정 혹은 판매자로 사용자가 로그인합니다.")
     @PostMapping("/auth/login")
     public ResponseEntity<?> login(@RequestBody LoginDto.Request loginDto) {
         LoginDto.Response response = memberService.login(loginDto);
@@ -67,6 +73,7 @@ public class MemberController {
     /**
      * 로그아웃
      **/
+    @ApiOperation(value = "로그아웃", notes = "사용자의 로그아웃")
     @PostMapping("/auth/logout")
     public ResponseEntity<?> logout(@RequestHeader("Authorization") String accessToken,
                                     @RequestHeader("RefreshToken") String refreshToken) {
@@ -78,6 +85,7 @@ public class MemberController {
     /**
      * Token reissue
      **/
+    @ApiOperation(value = "토큰 재발급", notes = "유효기간이 지난 토큰을 재발급합니다.")
     @PostMapping("/auth/refresh-token")
     public ResponseEntity<?> reissue(@RequestHeader("RefreshToken") String refreshToken) {
         return ResponseEntity.ok(memberService.reissue(refreshToken));
