27ff5c8353b82f9c3cbb65489a257c7ee27f3337
chandle <phc09188@gmail.com>
2022-12-12 21:28:47 +0900

    Fix - Auth TestCode 오류 해결 AuthUtil generateEmailAuthNum메소드 6자리가 반환되지 않는 경우 체크 Logout 필요 없는 RefreshToken을 헤더로 받고있던 문제 수정

--- src/main/java/com/minwonhaeso/esc/util/AuthUtil.java
+++ src/main/java/com/minwonhaeso/esc/util/AuthUtil.java
@@ -12,6 +12,13 @@ public class AuthUtil {
     public static String generateEmailAuthNum() {
         java.util.Random generator = new java.util.Random();
         generator.setSeed(System.currentTimeMillis());
-        return String.valueOf(generator.nextInt(1000000) % 1000000);
+        String result =  String.valueOf(generator.nextInt(1000000) % 1000000);
+        if(result.length() !=6){
+            for (int i = 0; i < 6 - result.length(); i++) {
+                result = "0" + result;
+            }
+        }
+        return result;
     }
+
 }
--- src/test/java/com/minwonhaeso/esc/member/service/MemberServiceWithPrincipalTest.java
+++ src/test/java/com/minwonhaeso/esc/member/service/MemberServiceWithPrincipalTest.java
@@ -80,7 +80,7 @@ class MemberServiceWithPrincipalTest {
         //then
         Member member = memberRepository.findByEmail(email)
                 .orElseThrow(() -> new AuthException(AuthErrorCode.MemberNotLogIn));
-        assertEquals(response.getName(), member.getName());
+        assertEquals(response.getNickName(), member.getNickname());
     }
 
     @Test
