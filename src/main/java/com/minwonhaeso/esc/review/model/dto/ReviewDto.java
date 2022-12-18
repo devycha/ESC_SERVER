1faeb7706d15e4bc14ba8cb70310d87030310d2c
dalgorithm <llsa0927@naver.com>
2022-12-18 20:43:20 +0900

    Fix : 리뷰 DTO 수정

--- src/main/java/com/minwonhaeso/esc/review/model/dto/ReviewDto.java
+++ src/main/java/com/minwonhaeso/esc/review/model/dto/ReviewDto.java
@@ -1,5 +1,6 @@
 package com.minwonhaeso.esc.review.model.dto;
 
+import com.minwonhaeso.esc.member.model.entity.Member;
 import com.minwonhaeso.esc.review.model.entity.Review;
 import io.swagger.annotations.ApiModel;
 import lombok.AllArgsConstructor;
@@ -7,7 +8,6 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 
@@ -29,23 +29,41 @@ public class ReviewDto {
     @ApiModel(value = "리뷰 Response Body")
     public static class Response {
         private Long id;
-        private Long memberId;
+        private MemberResponse member;
         private Double star;
         private String comment;
-        private String nickname;
-        private String imgUrl;
         private String createdAt;
 
         public static ReviewDto.Response fromEntity(Review review) {
             return Response.builder()
                     .id(review.getId())
-                    .memberId(review.getMember().getMemberId())
+                    .member(MemberResponse.fromEntity(review.getMember()))
                     .star(review.getStar())
                     .comment(review.getComment())
-                    .nickname(review.getMember().getNickname())
-                    .imgUrl(review.getMember().getImgUrl())
                     .createdAt(review.getCreatedAt().format(DateTimeFormatter
                             .ofPattern("yyyy-MM-dd a HH:mm").withLocale(Locale.forLanguageTag("ko"))))
+                    .build();
+        }
+    }
+
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Builder
+    private static class MemberResponse {
+        private Long id;
+        private String email;
+        private String nickname;
+        private String name;
+        private String imgUrl;
+
+        public static ReviewDto.MemberResponse fromEntity(Member member) {
+            return MemberResponse.builder()
+                    .id(member.getMemberId())
+                    .email(member.getEmail())
+                    .nickname(member.getNickname())
+                    .name(member.getName())
+                    .imgUrl(member.getImgUrl())
                     .build();
         }
     }
